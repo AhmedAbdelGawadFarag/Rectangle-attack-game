76d80aedbc574cb888a2f702efb6c20557f4bdcf
unknown <ahmed.abdelgawad.farag@gmail.com>
2019-08-21 18:19:53 +0200

    now u can kill anotehr rectangle

--- bullet.cpp
+++ bullet.cpp
@@ -1,5 +1,6 @@
 #include "bullet.h"
-
+#include<QList>
+#include<enemy.h>
 Bullet::Bullet()
 {
     QTimer *timer=new QTimer();
@@ -8,7 +9,7 @@ Bullet::Bullet()
 
     connect(timer,SIGNAL(timeout()),this,SLOT(move()));
 
-    timer->start(40);
+    timer->start(30);
 
 
 
@@ -16,6 +17,19 @@ Bullet::Bullet()
 
 void Bullet::move()
 {
+    QList<QGraphicsItem *> m=collidingItems();
+    for(int i=0;i<collidingItems().size();i++){
+        if(typeid (*(m[i]))==typeid (Enemy)){
+            this->scene()->removeItem(m[i]);
+            this->scene()->removeItem(this);
+            delete this;
+            delete m[i];
+            return;
+        }
+
+    }
+
+
     this->setPos(this->x(),this->y()-10);
     if(this->y()<0-this->rect().height()){
        this->scene()->removeItem(this);
